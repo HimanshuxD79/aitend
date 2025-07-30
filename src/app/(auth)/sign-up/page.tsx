1df465142c86caea6ddf7f5b425ff26d03f3cc30
HimanshuxD79 <himanshudiwan.work@gmail.com>
2025-07-30 20:30:42 +0530

    social-auth

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,15 +1,16 @@
-import { auth } from "@/lib/auth";
-import { HomeView } from "@/modules/home/ui/views/home-view";
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
 import { redirect } from "next/navigation";
+import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 
 const Page = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
-  if (!session) {
-    redirect("/sign-in");
+  if (!!session) {
+    redirect("/");
   }
-  return <HomeView />;
+  return <SignUpView />;
 };
+
 export default Page;
